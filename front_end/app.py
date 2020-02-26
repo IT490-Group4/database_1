f88907ef457d14e3ab4feda2b167664a26b8f7a6
kp482 <kp482@njit.edu>
2020-02-26 14:13:43 -0500

    updating app.py

--- front_end/app.py
+++ front_end/app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-        return 'Hello, World!'
+def index():
+        return render_template('index.html')
+
+@app.route('/submit')
+def submit():
+        return render_template('submit.html')
